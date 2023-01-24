a7d140fe5486e2957af80ac96d443603f16f643e
Valeriy Nefedov <nef1k@outlook.com>
2023-01-24 23:33:58 +0300

    Lesson XX - Prepare code for router

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div className={s.userInfo}>
         <img src="https://i.imgur.com/7MRSU4k.png" alt="Background" className={s.background}/>
         <img
