9886a720174ed9395efa14bdfad2280dc7b48384
Valeriy Nefedov <nef1k@outlook.com>
2023-01-25 11:32:21 +0300

    Lesson 20 - Implement routing, create StyledNavLink, replaced standard navlinks with styled ones

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header/>
-      <div className="content-wrapper">
-        <Navbar/>
-        <div className="content">
-          {/*<Profile />*/}
-          <Dialogs/>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header/>
+        <div className="content-wrapper">
+          <Navbar/>
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Profile/>}/>
+              <Route path="/dialogs" element={<Dialogs/>}/>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import s from './Navbar.module.css';
+import StyledNavLink from "../Utils/StyledNavLink";
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href="/dialogs">Messages</a>
-      </div>
-      <div className={`${s.item} ${s.active}`}>
-        <a href="/profile">Profile</a>
+        <StyledNavLink activeClassName={s.active} to="/" end>Profile</StyledNavLink>
       </div>
       <div className={s.item}>
-        <a href="#">News</a>
+        <StyledNavLink activeClassName={s.active} to="/dialogs">Messages</StyledNavLink>
       </div>
       <div className={s.item}>
-        <a href="#">Music</a>
+        <StyledNavLink activeClassName={s.active} to="/news">News</StyledNavLink>
+      </div>
+      <div className={s.item}>
+        <StyledNavLink activeClassName={s.active} to="/music">Music</StyledNavLink>
+      </div>
+      <div className={s.item}>
+        <StyledNavLink activeClassName={s.active} to="/settings">Settings</StyledNavLink>
       </div>
     </nav>
   );
